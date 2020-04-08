c28824059e9a8b9b048313b686f00aa2c3e3c902
Gaurav Datt <39988566+gauravdatt2000@users.noreply.github.com>
2020-04-08 13:09:35 +0530

    Add files via upload

---
+++ Template.cpp
@@ -0,0 +1,48 @@
+/*
+* Note: This template uses some c++11 functions , so you have to compile it with c++11 flag.
+*       Example:-   $ g++ -std=c++11 c++Template.cpp
+*
+* Author : Gaurav datt
+* Codechef Handle: gauravdatt2000
+*
+*/
+ 
+/********   All Required Header Files ********/
+	#include <bits/stdc++.h>
+	#include <cstdio>
+	#include <cstring>
+	#include <cmath>
+	#include <cstring>
+	#include <chrono>
+	#include <complex>
+	#define rf freopen("inputf.in", "r", stdin)
+	#define wf freopen("outputf.in", "w", stdout)
+	#define endl "\n"
+	#define ll long long int
+	#define vi vector<int>
+	#define vll vector<ll>
+	#define vvi vector < vi >
+	#define pii pair<int,int>
+	#define pll pair<long long, long long>
+	#define mod 1000000007
+	#define inf 1000000000000000001;
+	#define all(c) c.begin(),c.end()
+	#define mp(x,y) make_pair(x,y)
+	#define mem(a,val) memset(a,val,sizeof(a))
+	#define eb emplace_back
+	#define f first
+	#define s second
+	
+	using namespace std;
+	int main()
+	{
+		//std::ios::sync_with_stdio(false);
+		//rf; wf;
+		int T,a,b;
+		cin>>T;
+		// cin.ignore(); must be there when using getline(cin, s)
+		while(T--)
+		{
+		}
+		return 0;
+	}
